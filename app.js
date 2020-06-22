2023574a66c33d6731ab32b25adbebcd89e20e62
Lawrence <Lawrence4code@gmail.com>
2020-06-22 11:47:59 +0530

    add cors capability

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const postsRoutes = require("./routes/posts.js");
 const userRoutes = require("./routes/user.js");
@@ -46,6 +47,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("images")));
+app.use(cors());
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
